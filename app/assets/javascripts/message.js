eee3768cfc58b79e6a967c6186937a71659216bd
cocoon-code <k.kesusek_cocoon@icloud.com>
2020-01-19 14:23:36 +0900

    AutoUpdate

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -25,6 +25,47 @@ $(function(){
                   return html;
     };
   }
+  var updataHTML = function(message) {
+    if (message.content && message.image) {
+      var html = `<div class="message" data-message-id=` + message.id + `>` +
+        `<div class="message_talker">` +
+          message.user_name +
+          `<div class="message_date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="message_text">` +
+          message.content +
+          `<img src="` + message.image + `">` +
+        `</div>` +
+      `</div>`
+    } else if (message.content){
+      var html = `<div class="message" data-message-id=` + message.id + `>` +
+      `<div class="message_talker">` +
+        message.user_name +
+        `<div class="message_date">` +
+          message.created_at +
+        `</div>` +
+      `</div>` +
+      `<div class="message_text">` +
+        message.content +
+      `</div>` +
+    `</div>`
+    } else if (message.image) {
+      var html = `<div class="message" data-message-id=` + message.id + `>` +
+      `<div class="message_talker">` +
+        message.user_name +
+        `<div class="message_date">` +
+          message.created_at +
+        `</div>` +
+      `</div>` +
+      `<div class="message_text">` +
+        `<img src="` + message.image + `">` +
+      `</div>` +
+    `</div>`
+    };
+    return html;
+  };
   $('#new_message').on('submit',function(e){
     e.preventDefault();
     $('.submit_btn').removeAttr('data-disable-with');
@@ -48,4 +89,31 @@ $(function(){
       alert('メッセージを送信できません');
     });
   });
+  var reloadMessages = function() {
+    last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages){
+      if (messages.length != 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message){
+          insertHTML += updataHTML(message)
+        });
+        $('.main_chat__message-list').append(insertHTML);
+        $('.main_chat__message-list').animate({scrollTop: $('.main_chat__message-list')[0].scrollHeight});
+        $("#new_message")[0].reset();
+        $(".submit_btn").prop("disabled", false);
+      }
+    })
+    .fail(function(){
+      console.log('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
